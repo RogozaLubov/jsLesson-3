7b9d7cab0c7a498d4d33d7fdbfeb24ccdeda5654
Rogoza Lyuba <lyubashka22rogoza@gmail.com>
2021-08-11 19:00:15 +0300

    add task3

--- task1/script1.js
+++ task1/script1.js
@@ -1,6 +1,6 @@
 function factorial(n){
     return n ? n * factorial(n-1) :1 ;
 }
-alert (factorial(4));
+alert (factorial(3));
 
 
---
+++ task2/script2.js
@@ -0,0 +1,9 @@
+function pow(a,b){
+    
+    let  result =a;
+    let a = prompt("Enter a value");
+    for ( let i =1 ; i<b ; i++){
+        result +=a;
+    }
+    return result;
+}
